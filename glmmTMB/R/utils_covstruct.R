634298c9e7665f333b95866aead08216f5f94ccb
Pascal-Kueng <kueng.pascal@gmail.com>
2026-08-17 16:09:45 +0200

    Add parser for Kronecker covariance terms

--- glmmTMB/R/utils_covstruct.R
+++ glmmTMB/R/utils_covstruct.R
@@ -71,3 +71,60 @@ parseNumLevels <- function(levels) {
     ans
 }
 
+.kron_supported <- c("homdiag", "diag", "homcs", "cs", "us", "ar1",
+                     "hetar1")
+
+.kron_call_name <- function(x) {
+    if (!is.call(x)) return("")
+    deparse1(x[[1L]], collapse = "")
+}
+
+.kron_flatten <- function(x) {
+    while (is.call(x) && identical(.kron_call_name(x), "(") &&
+           length(x) == 2L) x <- x[[2L]]
+    if (is.call(x) && identical(.kron_call_name(x), "%x%") &&
+        length(x) == 3L) {
+        return(c(.kron_flatten(x[[2L]]), .kron_flatten(x[[3L]])))
+    }
+    list(x)
+}
+
+.kron_parse <- function(bar, add_arg) {
+    if (!is.call(bar) || !identical(.kron_call_name(bar), "|") ||
+        length(bar) != 3L) {
+        stop("kron() must contain a product random-effects term",
+             call. = FALSE)
+    }
+    if (!is.call(add_arg) || !identical(.kron_call_name(add_arg), "kron") ||
+        length(add_arg) != 1L) {
+        stop("kron() does not accept additional arguments", call. = FALSE)
+    }
+
+    margins <- .kron_flatten(bar[[2L]])
+    if (length(margins) < 2L) {
+        stop("kron() needs at least two margins joined by %x%",
+             call. = FALSE)
+    }
+
+    struc <- character(length(margins))
+    expr <- vector("list", length(margins))
+    margin_names <- character(length(margins))
+
+    for (i in seq_along(margins)) {
+        margin <- margins[[i]]
+        arg_names <- names(margin)[-1L]
+        has_named_arg <- length(arg_names) > 0L && any(nzchar(arg_names))
+        if (!is.call(margin) || length(margin) != 2L || has_named_arg) {
+            stop("kron() margins must look like us(1 + x) or ",
+                 "ar1(0 + time)", call. = FALSE)
+        }
+        struc[[i]] <- .kron_call_name(margin)
+        if (!struc[[i]] %in% .kron_supported) {
+            stop("unsupported kron() margin: ", struc[[i]], call. = FALSE)
+        }
+        expr[[i]] <- margin[[2L]]
+        margin_names[[i]] <- deparse1(expr[[i]], collapse = "")
+    }
+
+    list(struc = struc, expr = expr, margin_names = margin_names)
+}
---
+++ glmmTMB/tests/testthat/test-kron-parser.R
@@ -0,0 +1,73 @@
+stopifnot(require("testthat"), require("glmmTMB"))
+
+test_that("kron parser preserves the product specification", {
+    split <- reformulas::splitForm(
+        y ~ kron((us(0 + member) %x% ar1(0 + time)) %x%
+                     diag(0 + outcome) | site),
+        specials = "kron"
+    )
+    expect_identical(split$reTrmClasses, "kron")
+    expect_identical(
+        split$reTrmFormulas[[1L]],
+        quote((us(0 + member) %x% ar1(0 + time)) %x%
+                  diag(0 + outcome) | site)
+    )
+    expect_identical(split$reTrmAddArgs[[1L]], quote(kron()))
+
+    spec <- glmmTMB:::.kron_parse(
+        split$reTrmFormulas[[1L]],
+        split$reTrmAddArgs[[1L]]
+    )
+
+    expect_identical(spec$struc, c("us", "ar1", "diag"))
+    expect_identical(
+        spec$expr,
+        list(quote(0 + member), quote(0 + time), quote(0 + outcome))
+    )
+    expect_identical(
+        spec$margin_names,
+        c("0 + member", "0 + time", "0 + outcome")
+    )
+    expect_identical(
+        lengths(spec[c("struc", "expr", "margin_names")]),
+        c(struc = 3L, expr = 3L, margin_names = 3L)
+    )
+})
+
+test_that("kron parser rejects malformed product terms", {
+    expect_error(
+        glmmTMB:::.kron_parse(
+            quote(us(0 + member) %x% ar1(0 + time)),
+            quote(kron())
+        ),
+        "must contain a product random-effects term"
+    )
+    expect_error(
+        glmmTMB:::.kron_parse(
+            quote(us(0 + member) | group),
+            quote(kron())
+        ),
+        "needs at least two margins"
+    )
+    expect_error(
+        glmmTMB:::.kron_parse(
+            quote(toep(0 + member) %x% ar1(0 + time) | group),
+            quote(kron())
+        ),
+        "unsupported kron\\(\\) margin: toep"
+    )
+    expect_error(
+        glmmTMB:::.kron_parse(
+            quote(us(x = 0 + member) %x% ar1(0 + time) | group),
+            quote(kron())
+        ),
+        "margins must look like"
+    )
+    expect_error(
+        glmmTMB:::.kron_parse(
+            quote(us(0 + member) %x% ar1(0 + time) | group),
+            quote(kron(scale = "global"))
+        ),
+        "does not accept additional arguments"
+    )
+})
